6b8ef826c9623f3d9e2e8f065986a73d24d6512f
Jorge Patrocínio <141787356+jorgesantos001@users.noreply.github.com>
2024-05-02 20:17:19 -0300

    Add files via upload

---
+++ Pagina Login/index.js
@@ -0,0 +1,39 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            loginRealizado: false,
+            avisoLogin: '',
+            cadastroRealizado: false,
+            avisoCadastro: '',
+            username: '',
+            password: '',
+        };
+    },
+    methods: {
+        login() {
+            console.log("Clicou em Login");
+            if (this.username && this.password) {
+                this.loginRealizado = true;
+                this.avisoLogin = "Login realizado";
+            } else {
+                this.avisoLogin = "Por favor, insira o email e a senha.";
+            }
+        },
+        cadastrar() {
+            console.log("Clicou em Cadastrar");
+            if (this.username && this.password) {
+                this.cadastroRealizado = true;
+                this.avisoCadastro = "Cadastro realizado";
+            } else {
+                this.avisoLogin = "Por favor, insira o email e a senha.";
+            }
+        },
+        limparAviso() {
+            this.avisoLogin = ''; // Limpa o aviso quando os campos de entrada são preenchidos
+        }
+    }
+});
+app.mount("#app");
+
+
+
